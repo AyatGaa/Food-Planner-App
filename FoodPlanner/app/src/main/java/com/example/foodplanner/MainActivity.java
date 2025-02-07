1c3ec00107dd4126ded9652c45f75fbc63996ac9
Ayat Gamal <ayat.ga106@gmail.com>
2025-02-07 17:10:49 +0200

    add sign in page

--- FoodPlanner/app/src/main/java/com/example/foodplanner/MainActivity.java
+++ FoodPlanner/app/src/main/java/com/example/foodplanner/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_sign_up);
+        setContentView(R.layout.fragment_splash_screen);
     }
 }
